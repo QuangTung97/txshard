445c7f2bb7d8f34aff2aefd225ab899656df7c17
Tùng Tạ Quang <tung.tq@teko.vn>
2021-01-08 14:13:59 +0700

    Add Processor

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module txshard
 
 go 1.14
 
-require github.com/stretchr/testify v1.6.1
+require (
+	github.com/stretchr/testify v1.6.1
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.16.0
+)
--- txshard.go
+++ txshard.go
@@ -2,6 +2,9 @@ package txshard
 
 import (
 	"context"
+	"go.uber.org/zap"
+	"sync"
+	"time"
 )
 
 // Revision ...
@@ -67,10 +70,109 @@ type EtcdClient interface {
 	CompareAndSet(ctx context.Context, kvs []CASKeyValue) error
 }
 
+// Runner ...
+type Runner func(ctx context.Context, partitionID PartitionID)
+
+type activeRunner struct {
+	cancel context.CancelFunc
+}
+
 // Processor ...
 type Processor struct {
+	state  *state
+	client EtcdClient
+	runner Runner
+	logger *zap.Logger
+
+	timeoutDuration time.Duration
+
+	leaseChan     <-chan LeaseID
+	nodeChan      <-chan NodeEvent
+	partitionChan <-chan PartitionEvents
+	leaderChan    <-chan LeaderEvent
+
+	activeMap map[PartitionID]activeRunner
+	wg        sync.WaitGroup
+}
+
+// Config ...
+type Config struct {
+	PartitionCount    PartitionID
+	PartitionPrefix   string
+	NodePrefix        string
+	SelfNodeID        NodeID
+	SelfLastPartition PartitionID
+
+	Client EtcdClient
+	Runner Runner
+	Logger *zap.Logger
+
+	LeaseChan     <-chan LeaseID
+	NodeChan      <-chan NodeEvent
+	PartitionChan <-chan PartitionEvents
+	LeaderChan    <-chan LeaderEvent
+}
+
+// NewProcessor ...
+func NewProcessor(conf Config) *Processor {
+	st := newState(
+		conf.PartitionCount, conf.PartitionPrefix, conf.NodePrefix,
+		conf.SelfNodeID, conf.SelfLastPartition,
+	)
+
+	timeout := 60 * time.Second
+
+	return &Processor{
+		state:  st,
+		client: conf.Client,
+		runner: conf.Runner,
+		logger: conf.Logger,
+
+		timeoutDuration: timeout,
+
+		leaseChan:     conf.LeaseChan,
+		nodeChan:      conf.NodeChan,
+		partitionChan: conf.PartitionChan,
+		leaderChan:    conf.LeaderChan,
+	}
 }
 
 // Run ...
 func (p *Processor) Run(ctx context.Context) {
+	var after <-chan time.Time
+	for {
+		output := p.state.runLoop(ctx, p.leaseChan, p.nodeChan, p.partitionChan, p.leaderChan, after)
+		if ctx.Err() != nil {
+			p.wg.Wait()
+			return
+		}
+
+		if len(output.kvs) > 0 {
+			err := p.client.CompareAndSet(ctx, output.kvs)
+			if err != nil {
+				p.logger.Error("client.CompareAndSet", zap.Any("kvs", output.kvs))
+				after = time.After(p.timeoutDuration)
+				continue
+			}
+		}
+
+		for _, partition := range output.startPartitions {
+			p.wg.Add(1)
+			ctx, cancel := context.WithCancel(ctx)
+
+			go func() {
+				defer p.wg.Done()
+				p.runner(ctx, partition)
+			}()
+
+			p.activeMap[partition] = activeRunner{
+				cancel: cancel,
+			}
+		}
+
+		for _, partition := range output.stopPartitions {
+			p.activeMap[partition].cancel()
+			delete(p.activeMap, partition)
+		}
+	}
 }
